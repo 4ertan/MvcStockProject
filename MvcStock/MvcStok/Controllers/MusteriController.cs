87ba084c05ebe2a88801944a8f71081edca49e78
4ertan <ertanosmanoglu13@gmail.com>
2022-05-01 19:08:59 +0300

    Mvc Stock

---
+++ MvcStock/MvcStok/Controllers/MusteriController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+namespace MvcStok.Controllers
+{
+    public class MusteriController : Controller
+    {
+        // GET: Musteri
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index(string p)
+        {
+            var degerler=from d in db.TBLMUSTERILER select d;
+            if(!string.IsNullOrEmpty(p))
+            {
+                degerler = degerler.Where(m => m.MUSTERIAD.Contains(p));
+            }
+            return View(degerler.ToList());
+            //var degerler = db.TBLMUSTERILER.ToList();
+            //return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniMusteri()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniMusteri(TBLMUSTERILER p1)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("YeniMusteri");
+            }
+            db.TBLMUSTERILER.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult SIL(int id)
+        {
+            var must = db.TBLMUSTERILER.Find(id);
+            db.TBLMUSTERILER.Remove(must);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult MusteriGetir(int id)
+        {
+            var mus = db.TBLMUSTERILER.Find(id);
+            return View("MusteriGetir",mus);
+        }
+        public ActionResult Guncelle(TBLMUSTERILER p1)
+        {
+            var mgn = db.TBLMUSTERILER.Find(p1.MUSTERIID);
+            mgn.MUSTERIAD = p1.MUSTERIAD;
+            mgn.MUSTERISOYAD = p1.MUSTERISOYAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
